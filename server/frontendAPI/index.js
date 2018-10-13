fe15c581290e7b054bf509e6f831218987b13f68
Jeffrey Bissel <bissel@uni-bremen.de>
2018-10-13 18:28:42 +0200

    [fix][API frontend] cross origin access

--- server/frontendAPI/index.js
+++ server/frontendAPI/index.js
@@ -9,7 +9,7 @@ var con = new Database(config.mysql);
 http.createServer(function (req, res) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://' +config.url+ ':'+config.port);
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
