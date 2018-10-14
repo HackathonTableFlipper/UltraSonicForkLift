02189e6d41184a990ce17f6a3ec77eb231dcc7b0
Jan Möhlmann <janmoehl@uni-bremen.de>
2018-10-14 11:10:16 +0200

    WEBSERVER: graph

--- frontend/web/webserver/app/controllers/graph_controller.rb
+++ frontend/web/webserver/app/controllers/graph_controller.rb
@@ -1,3 +1,5 @@
+include JsonHelper
+include OverviewHelper
 class GraphController < ApplicationController
 
 =begin def update
@@ -51,19 +53,62 @@ class GraphController < ApplicationController
   end
 =end
 
-  end
-  include GraphHelper
-
   def list
-    @graphEntries = {
-        "type" => "ListData",
-	      "data" => [{
+    if (Rails.configuration.ids == nil || Rails.configuration.ids.empty?)
+      @ids = getAllDevices
+    else
+      @ids = Rails.configuration.ids
+    end
+    if (Rails.configuration.startTime == nil)
+      @startTime = "2018-10-13 00:00:00"
+    else
+      @startTime = Rails.configuration.startTime
+    end
+    if (Rails.configuration.endTime == nil)
+      @endTime = "2999-01-01 00:00:00"
+    else
+      @endTime = Rails.configuration.endTime
+    end
+    begin
+      response = getJsonForType({:type => "ListData",
+                    "devices" => @ids,
+                    "start" => @startTime,
+                    "end" => @endTime})
+    rescue Errno::EHOSTUNREACH
+      redirect_to settings_edit_path
+      return
+    end
+
+    if response["type"] != "ListData"
+      raise "Wrong return type"
+    else
+      @dataEntries = response["data"]
+      @dataEntries.each do |device|
+        transDate = []
+        device["loadedTimes"].each do |entry|
+          transDate.push([toTimeString(entry[0]), toTimeString(entry[1])])
+        end
+        device["loadedTimes"] = transDate
+        transDate = []
+        device["emptyTimes"].each do |entry|
+          transDate.push([toTimeString(entry[0]), toTimeString(entry[1])])
+        end
+        device["emptyTimes"] = transDate
+      end
+    end
+=begin
+    @dataEntries = [{
             "id" => "00112233",
             "name" => "stapler1",
             "location" => "Tisch 2",
-            "loadedTimes": [["1801, 2, 4, 12, 10, 00", "1801, 2, 4, 12, 10, 00"]],
-            "emptyTimes":  []
-        },
-        ...
+            "loadedTimes" => [["1801, 2, 4, 12, 10, 00", "1801, 2, 4, 12, 15, 00"]],
+            "emptyTimes" => [["1801, 2, 4, 14, 10, 00", "1801, 2, 4, 14, 15, 00"]]
+        }]
+=end
   end
+
+  def toTimeString(unformatted) 
+    return unformatted.gsub(/[-T:]/, ", ")[0,24]
+  end
+
 end
